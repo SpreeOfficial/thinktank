4f79484e9900690201dd5c809edebde4f15fa172
SpreeOfficial <Ronnilou@hotmail.com>
2026-04-27 23:14:23 +0200

    Update lobby.js

--- public/lobby.js
+++ public/lobby.js
@@ -25,23 +25,3 @@ socket.on("updatePlayers", (players) => {
 socket.on("gameStarted", () => {
   window.location = `/game.html?lobby=${lobbyId}`;
 });
-
-function handleJoin() {
-  const name = document.getElementById("name").value;
-  joinLobby(name);
-}
-
-function handleStart() {
-  startGame();
-}
-
-function updateLobbyUI(players) {
-  const list = document.getElementById("players");
-  list.innerHTML = "";
-
-  Object.values(players).forEach(p => {
-    const el = document.createElement("div");
-    el.innerText = `${p.name} - score: ${p.score}`;
-    list.appendChild(el);
-  });
-}
